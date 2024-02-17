9630742e44e8ed8b8f59e39c5b75e11571445e3d
Kiana Nb <kiana.nb34@gmail.com>
2024-02-17 19:35:58 +0330

    data fetch done

--- frontend/app/category/page.tsx
+++ frontend/app/category/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -8,24 +10,47 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-const page = () => {
+interface WordData {
+  word: string;
+  // Define other properties if present in the actual response data
+}
+
+const Page = () => {
+  const [wordData, setWordData] = useState<WordData>({ word: "" });
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8000/api/v1/word");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setWordData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
   return (
     <main className="p-5 flex justify-center flex-col text-center gap-5">
       <h1> انتخاب دسته‌بندی</h1>
       <button> تصادفی</button>
       <section>
         <Dialog>
-          <DialogTrigger> غذاها</DialogTrigger>
+          <DialogTrigger>{wordData.word}</DialogTrigger>
           <DialogContent>
             <DialogHeader>
               <DialogTitle> انتخاب سطح</DialogTitle>
-              <DialogDescription >
-              <div className="flex flex-col gap-5 p-5">
-              <button> سطح ۱</button>
-              <button> سطح ۲</button>
-              <button> سطح ۳</button>
-              </div>
-               
+              <DialogDescription>
+                <div className="flex flex-col gap-5 p-5">
+                  <button> سطح ۱</button>
+                  <button> سطح ۲</button>
+                  <button> سطح ۳</button>
+                </div>
               </DialogDescription>
             </DialogHeader>
           </DialogContent>
@@ -35,4 +60,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
