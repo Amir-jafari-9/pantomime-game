d0475c6b95882f3297f4595434555076ad1c219c
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-17 16:11:07 +0330

    add new route and notFound to main file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+const wordRoute = require("./routes/word");
+const notFound = require("./middleware/not-found");
+
+// handel req.body
+app.use(express.json());
+
+// Routes
+app.use("/api/v1", wordRoute);
+
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
