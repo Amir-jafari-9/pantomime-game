3a315f269224c1b991a1e3cf46961cf7a78ccc5a
Kiana Nb <kiana.nb34@gmail.com>
2024-02-17 19:32:48 +0330

    data fetch done

--- backend/src/controller/word.js
+++ backend/src/controller/word.js
@@ -1,12 +1,13 @@
 const getWord = (req, res) => {
-    res.status(200).json({
-        status: 200,
-        success: true,
-        message: "get word successfully",
-        data: {
-            word: "cat"
-        }
-    });
+  console.log("hi");
+  res.status(200).json({
+    status: 200,
+    success: true,
+    message: "get word successfully",
+    data: {
+      word: "cat",
+    },
+  });
 };
 
 module.exports = { getWord };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 const wordRoute = require("./routes/word");
 const notFound = require("./middleware/not-found");
 
 // handel req.body
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/v1", wordRoute);
