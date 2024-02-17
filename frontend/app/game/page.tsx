89a14100519ec49d3dc69ec8ea4b766fadcb5a59
Kiana Nb <kiana.nb34@gmail.com>
2024-02-17 17:01:46 +0330

    added content

---
+++ frontend/app/game/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className="p-5 flex justify-center flex-col text-center gap-5">
+        <h1> دسته‌بندی غذا | امتیاز ۵ </h1>
+        <section>
+            <p> کلمه </p>
+        </section>
+
+        
+    </main>
+  )
+}
+
+export default Page
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div> yo </div>
-  );
+  return <div> yo </div>;
 }
