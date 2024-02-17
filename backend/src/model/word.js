907806c4b21ee72718d63af75357ce2977a2b2dd
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-17 18:06:04 +0330

    create scheama for word

---
+++ backend/src/model/word.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const WordSchema = new mongoose.Schema({
+    word: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ["easy", "normal", "hard"]
+    }
+});
+const CategorySchema = new mongoose.Schema({
+    categoryName: { type: String, required: true },
+    words: [WordSchema]
+});
+
+const CategoryModel = mongoose.model("Category", CategorySchema);
+
+module.exports = CategoryModel;
