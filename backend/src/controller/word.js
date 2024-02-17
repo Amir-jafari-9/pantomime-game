487768ce10b77609f35658974b1a9e914f4edd34
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-17 18:07:24 +0330

    add endpoint for get a word

--- backend/src/controller/word.js
+++ backend/src/controller/word.js
@@ -1,12 +1,36 @@
-const getWord = (req, res) => {
+const CategoryModel = require("../model/word");
+
+const getWord = async (req, res) => {
+    const { category, difficulty } = req.body;
+    const categories = await CategoryModel.find({ categoryName: category });
+
+    const filterByDifficulty = categories
+        .filter((category) => category.words[0].difficulty === difficulty)
+        .map((word) => word.words[0].word);
+
     res.status(200).json({
         status: 200,
         success: true,
         message: "get word successfully",
         data: {
-            word: "cat"
+            category: category,
+            difficulty: difficulty,
+            word: filterByDifficulty
+        }
+    });
+};
+const addWord = async (req, res) => {
+    const word = await CategoryModel.create(req.body);
+    res.status(200).json({
+        status: 201,
+        success: true,
+        message: "add word successfully",
+        data: {
+            category: word.categoryName,
+            difficulty: word.words[0].difficulty,
+            word: word.words[0].word
         }
     });
 };
 
-module.exports = { getWord };
+module.exports = { getWord, addWord };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoCon = require("./connection/mongoCon");
 const port = process.env.PORT || 3000;
 
-const wordRoute = require("./routes/word");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error_handler");
+
+const wordRoute = require("./routes/word");
 
 // handel req.body
 app.use(express.json());
@@ -12,8 +15,19 @@ app.use(express.json());
 // Routes
 app.use("/api/v1", wordRoute);
 
+// middleware
+app.use(errorHandlerMiddleware);
 app.use(notFound);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+const start = async () => {
+    try {
+        await mongoCon(
+            process.env.MONGO_URI,
+            console.log("database connected ...")
+        );
+        app.listen(port, console.log(`server is listening to port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
