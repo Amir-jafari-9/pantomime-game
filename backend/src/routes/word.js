ca267368d2ed2bb2169dad45a5ae323854dcd5c3
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-18 10:57:34 +0330

    hot add and fix

--- backend/src/routes/word.js
+++ backend/src/routes/word.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getWord, addWord } = require("../controller/word");
+const { getWord, addWord, test } = require("../controller/word");
 
 router.route("/word").get(getWord).post(addWord);
+router.route("/test").get(test);
 
 module.exports = router;
