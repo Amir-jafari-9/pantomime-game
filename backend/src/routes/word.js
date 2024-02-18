7fcf243df5358798e571a32e1494478a95ba9277
Kiana Nb <kiana.nb34@gmail.com>
2024-02-18 12:17:45 +0330

    ui done done

--- backend/src/routes/word.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getWord } = require("../controller/word");
-
-router.route("/word").get(getWord);
-
-module.exports = router;
